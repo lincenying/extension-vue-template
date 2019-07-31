f7c4def28a4fa8c263a6e65a97c05af407d122dc
林岑影 <lincenying@qq.com>
2019-07-31 20:42:20 +0800

    up

--- src/content/App.vue
+++ src/content/App.vue
@@ -4,7 +4,7 @@
             <comp-table></comp-table>
             <span slot="footer" class="dialog-footer">
                 <el-button @click="showTable = false">取 消</el-button>
-                <el-button type="primary" @click="showTable = false">确 定</el-button>
+                <el-button type="primary" @click="handleSubmit">确 定</el-button>
             </span>
         </el-dialog>
     </div>
@@ -25,6 +25,18 @@ export default {
     methods: {
         toggleTable(status) {
             this.showTable = status
+        },
+        handleSubmit() {
+            this.$msgbox.alert('这是一段内容', '标题名称', {
+                confirmButtonText: '确定',
+                callback: action => {
+                    this.showTable = false
+                    this.$message({
+                        type: 'info',
+                        message: `action: ${action}`
+                    })
+                }
+            })
         }
     }
 }
--- src/content/components/test.vue
+++ src/content/components/test.vue
@@ -12,15 +12,6 @@ export default {
     },
     methods: {
         handleClick() {
-            // this.$msgbox.alert('这是一段内容', '标题名称', {
-            //     confirmButtonText: '确定',
-            //     callback: action => {
-            //         this.$message({
-            //             type: 'info',
-            //             message: `action: ${action}`
-            //         })
-            //     }
-            // })
             this.$app.toggleTable(true)
         }
     }
